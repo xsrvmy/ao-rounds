8668af4c3f56f67ef90aaa18dd633fa424f962e0
Rui Ming Xiong <xsrvmy@gmail.com>
2020-12-22 21:59:50 -0700

    Make AverageInfoCard a function and fix a warning

--- src/components/stats/AverageInfoCard.js
+++ src/components/stats/AverageInfoCard.js
@@ -2,52 +2,50 @@ import React from "react";
 import { Button, Card } from "react-bootstrap";
 import { centiToDisplayTime } from "../../timeUtils";
 
-export default class AverageInfoCard extends React.Component {
-  render() {
-    const { isBest, isWorst, average, times, isTrimmed } = this.props;
-    let minIndex = 0;
-    let maxIndex = 0;
-    times.forEach((time, i) => {
-      console.log(typeof i);
-      if (time > times[maxIndex]) maxIndex = i;
-      if (time < times[minIndex]) minIndex = i;
-    });
-    if (minIndex === maxIndex) maxIndex = 1;
+export default function AverageInfoCard(props) {
+  const { isBest, isWorst, average, times, isTrimmed } = props;
+  let minIndex = 0;
+  let maxIndex = 0;
+  times.forEach((time, i) => {
+    if (time > times[maxIndex]) maxIndex = i;
+    if (time < times[minIndex]) minIndex = i;
+  });
+  if (minIndex === maxIndex) maxIndex = 1;
 
-    return (
-      <Card>
-        <Card.Body>
-          <Card.Title>
-            <a
-              href="#123"
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title>
+          <a
+            href="#123"
+            className={
+              isBest ? "text-success" : isWorst ? "text-danger" : undefined
+            }
+          >
+            {isNaN(average) ? "Incomplete" : centiToDisplayTime(average)}
+          </a>
+        </Card.Title>
+        {times.map((time, i) => {
+          return (
+            <Button
+              key={i}
               className={
-                isBest ? "text-success" : isWorst ? "text-danger" : undefined
+                i === maxIndex
+                  ? "text-success"
+                  : i === minIndex
+                  ? "text-danger"
+                  : undefined
               }
+              variant="link"
+              size="sm"
             >
-              {isNaN(average) ? "Incomplete" : centiToDisplayTime(average)}
-            </a>
-          </Card.Title>
-          {times.map((time, i) => {
-            return (
-              <Button
-                className={
-                  i === maxIndex
-                    ? "text-success"
-                    : i === minIndex
-                    ? "text-danger"
-                    : undefined
-                }
-                variant="link"
-                size="sm"
-              >
-                {isTrimmed && (i === maxIndex || i === minIndex)
-                  ? `(${centiToDisplayTime(time)})`
-                  : centiToDisplayTime(time)}
-              </Button>
-            );
-          })}
-        </Card.Body>
-      </Card>
-    );
-  }
+              {isTrimmed && (i === maxIndex || i === minIndex)
+                ? `(${centiToDisplayTime(time)})`
+                : centiToDisplayTime(time)}
+            </Button>
+          );
+        })}
+      </Card.Body>
+    </Card>
+  );
 }
