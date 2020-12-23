ee9f26d0bd7a9e1895d9e651e014d1736ac37586
Rui Ming Xiong <xsrvmy@gmail.com>
2020-12-22 22:09:32 -0700

    Use hooks for ScramblePane

--- src/components/timer/ScramblePane.js
+++ src/components/timer/ScramblePane.js
@@ -1,76 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Card, Modal } from "react-bootstrap";
 
-export default class ScramblePane extends React.Component {
-  state = {
-    showLoadModal: false,
-    scrambleInput: "",
+export default function ScramblePane(props) {
+  const [isLoadModalShown, setLoadModalShown] = useState(false);
+  const [scrambleInput, setScrambleInput] = useState("");
+
+  const onLoadClick = () => {
+    setLoadModalShown(true);
+    setScrambleInput("");
   };
 
-  onLoadClick = () => {
-    this.setState({
-      showLoadModal: true,
-      scrambleInput: "",
-    });
+  const loadScrambles = () => {
+    console.log(scrambleInput);
+    setLoadModalShown(false);
   };
 
-  loadScrambles = () => {
-    console.log(this.state.scrambleInput);
-    this.setState({
-      showLoadModal: false,
-    });
-  };
-
-  render() {
-    const { scramble } = this.props;
-    return (
-      <>
-        <Card className="h-100">
-          <Card.Body>
-            <div className="text-monospace" style={{ fontSize: "14pt" }}>
-              {scramble}
-            </div>
-            <div className="text-right">
-              <Button
-                variant="outline-primary"
-                size="sm"
-                className="mr-2"
-                onClick={this.onLoadClick}
-              >
-                Load
-              </Button>
-              <Button
-                variant="outline-primary"
-                size="sm"
-                className="mr-2"
-                disabled
-              >
-                Previous
-              </Button>
-              <Button variant="outline-primary" size="sm">
-                Next
-              </Button>
-            </div>
-          </Card.Body>
-        </Card>
-        <Modal
-          show={this.state.showLoadModal}
-          onHide={() => this.setState({ showLoadModal: false })}
-        >
-          <Modal.Header closeButton>
-            <Modal.Title>Paste scrambles here</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <textarea
-              value={this.state.scrambleInput}
-              onChange={(e) => this.setState({ scrambleInput: e.target.value })}
-            ></textarea>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={this.loadScrambles}>Load</Button>
-          </Modal.Footer>
-        </Modal>
-      </>
-    );
-  }
+  const { scramble } = props;
+  return (
+    <>
+      <Card className="h-100">
+        <Card.Body>
+          <div className="text-monospace" style={{ fontSize: "14pt" }}>
+            {scramble}
+          </div>
+          <div className="text-right">
+            <Button
+              variant="outline-primary"
+              size="sm"
+              className="mr-2"
+              onClick={onLoadClick}
+            >
+              Load
+            </Button>
+            <Button
+              variant="outline-primary"
+              size="sm"
+              className="mr-2"
+              disabled
+            >
+              Previous
+            </Button>
+            <Button variant="outline-primary" size="sm">
+              Next
+            </Button>
+          </div>
+        </Card.Body>
+      </Card>
+      <Modal show={isLoadModalShown} onHide={() => setLoadModalShown(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Paste scrambles here</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <textarea
+            value={scrambleInput}
+            onChange={(e) => setScrambleInput(e.target.value)}
+          ></textarea>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={loadScrambles}>Load</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
